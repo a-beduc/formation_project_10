36f63f2858aa47cbbb0c38761eebc849552b0848
Alexandre Beduc <alexandre.beduc@gmail.com>
2025-02-28 11:15:05 +0100

    User urls path

--- OC_projet_10/urls.py
+++ OC_projet_10/urls.py
@@ -16,7 +16,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+from myauth.views import UserViewset
+
+
+router = routers.SimpleRouter()
+
+router.register('user', UserViewset, basename='user')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/', include(router.urls)),
 ]
