36771c85c4a5b1407ebd491a7bbc1e2136c15c77
Alexandre Beduc <alexandre.beduc@gmail.com>
2025-02-28 11:13:28 +0100

    Refactoring

--- myauth/serializers.py
+++ myauth/serializers.py
@@ -1,8 +1,14 @@
-from rest_framework.serializers import ModelSerializer
-from .models import User
+from rest_framework.serializers import ModelSerializer, ValidationError
+from myauth.models import User
 
 
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'date_of_birth', 'can_be_contacted', 'can_data_be_shared', 'created_time']
+        fields = ['id',
+                  'username',
+                  'date_of_birth',
+                  'can_be_contacted',
+                  'can_data_be_shared',
+                  'created_time'
+                  ]
