23c537379210e211c391a278f8ddf3a022c2c63b
Alexandre Beduc <alexandre.beduc@gmail.com>
2025-03-04 09:42:00 +0100

    Modification structure of API + Different information displayed for list or detail display + Addition of link to implement HATEOAS + Nesting of resources

--- OC_projet_10/settings.py
+++ OC_projet_10/settings.py
@@ -132,5 +132,5 @@ AUTH_USER_MODEL = 'myauth.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 2,
+    'PAGE_SIZE': 5,
 }
