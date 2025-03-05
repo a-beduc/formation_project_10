489c8ec0a7b3089a8be39cdc81b4985a768c2dc3
Alexandre Beduc <alexandre.beduc@gmail.com>
2025-03-05 09:17:21 +0100

    Configuration DRF Simple JWT

--- OC_projet_10/urls.py
+++ OC_projet_10/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_nested import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from myauth.views import UserViewset
 from softdesk.views import ProjectViewset, ContributorViewset, IssueViewset, CommentViewset
@@ -22,6 +23,8 @@ issues_router.register(r'comments', CommentViewset, basename='issue-comment')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/v1/', include('rest_framework.urls')),
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/v1/', include(router.urls)),
     path('api/v1/', include(projects_router.urls)),
     path('api/v1/', include(issues_router.urls)),
