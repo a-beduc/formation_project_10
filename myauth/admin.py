c7c3fb615f7ab839c3bf7dd9f4f95d0e8a524ca1
Alexandre Beduc <alexandre.beduc@gmail.com>
2025-03-13 23:34:39 +0100

    Addition custom display in admin panels

--- myauth/admin.py
+++ myauth/admin.py
@@ -1,3 +1,14 @@
-from django.contrib import admin
+from atexit import register
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from myauth.models import User
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = ('id', 'username', 'date_of_birth', 'can_be_contacted', 'can_data_be_shared')
+    search_fields = ('id', 'username')
+    ordering = ('id',)
+    list_filter = ('can_be_contacted', 'can_data_be_shared')
--- softdesk/admin.py
+++ softdesk/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from softdesk.models import Project, Contributor, Issue, Comment
 
-# Register your models here.
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'title', 'type')
+    list_filter = ('type',)
+    search_fields = ('title', 'author')
+
+
+@admin.register(Contributor)
+class ContributorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'project')
+    search_fields = ('user', 'project')
+
+
+@admin.register(Issue)
+class IssueAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'project', 'title', 'status', 'priority', 'type')
+    list_filter = ('status', 'priority', 'type')
+    search_fields = ('author', 'project', 'title')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'issue', 'short_content')
+    search_fields = ('author', 'issue')
+
+    def short_content(self, obj):
+        if obj.content:
+            return obj.content[:60] + ("..." if len(obj.content) > 60 else '')
+        return ''
