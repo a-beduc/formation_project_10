8b2eb70f5f036ffc10b81a193ff2a723cf622d63
Alexandre Beduc <alexandre.beduc@gmail.com>
2025-03-13 23:31:26 +0100

    Refactoring UserView permissions + Only the admin can create users + Addition of a custom error message

--- myauth/views.py
+++ myauth/views.py
@@ -1,7 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticated
 
-from myauth.models import User
+from django.contrib.auth import get_user_model
 from myauth.serializers import (UserListSerializer,
                                 UserDetailSerializer,
                                 UserCreateSerializer,
@@ -9,15 +9,33 @@ from myauth.serializers import (UserListSerializer,
 
 from myauth.permissions import IsAdminAuthenticated, IsOwner
 
+User = get_user_model()
+
 
 class UserViewset(ModelViewSet):
+    """
+    The SoftDesk API is a RESTful API built using Django Rest Framework with the objective
+    to develop a secured and efficient backend interface to serve different front-end applications.
+    The API use Json Web Token to define access permissions for the resources.
+
+    The Users resources can be used by users to access their own profiles and edit its content. It may
+    also be used to display all the users id and username.
+
+    A filter can be used to ease the search for a specific user id or username.
+
+    If you want to see the list of all the projects, please refer to the [projects endpoint](/api/v1/projects/).
+    """
     serializer_class = UserListSerializer
     serializer_map = {
         'list': UserListSerializer,
         'create': UserCreateSerializer,
         'update': UserUpdateSerializer,
-        'retrieve': UserDetailSerializer,
         'partial_update': UserUpdateSerializer,
+        'retrieve': UserDetailSerializer
+    }
+    permission_map = {
+        'list': [IsAuthenticated],
+        'create': [IsAdminAuthenticated]
     }
 
     def get_queryset(self):
@@ -30,13 +48,13 @@ class UserViewset(ModelViewSet):
             return super().get_serializer_class()
 
     def get_permissions(self):
-        if self.action in [
-            'retrieve',
-            'update',
-            'partial_update',
-            'destroy',
-        ]:
-            permission_classes = [IsOwner | IsAdminAuthenticated]
+        permission_classes = [IsAuthenticated]
+        if self.action in self.permission_map.keys():
+            permission_classes += self.permission_map.get(self.action, [])
         else:
-            permission_classes = [AllowAny]
+            permission_classes += self.default_permission
         return [permission() for permission in permission_classes]
+
+    def permission_denied(self, request, message=None, code=None):
+        message = "Vous n'avez pas la permission d'accéder à cette ressource."
+        super().permission_denied(request, message=message, code=code)
