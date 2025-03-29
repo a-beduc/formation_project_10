2a53a18640cc4d62ad66fb89b4b8d3608b0e0866
Alexandre Beduc <alexandre.beduc@gmail.com>
2025-03-29 21:42:37 +0100

    Adding the softdesk models fields to the admin panel for the User model (date_of_birth, can_data_be_shared, can_be_contacted)

--- src/myauth/admin.py
+++ src/myauth/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
 
 from myauth.models import User
 
 
 @admin.register(User)
-class UserAdmin(UserAdmin):
+class UserAdmin(DefaultUserAdmin):
     """
     Custom admin configuration for the User model.
     """
@@ -14,3 +14,9 @@ class UserAdmin(UserAdmin):
     search_fields = ('id', 'username')
     ordering = ('id',)
     list_filter = ('can_be_contacted', 'can_data_be_shared')
+    fieldsets = (DefaultUserAdmin.fieldsets +
+                 (("Softdesk API", {"fields": (
+                     "date_of_birth",
+                     "can_be_contacted",
+                     "can_data_be_shared",
+                 )}),))
