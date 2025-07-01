e38d41549c2734a77524cc9261d86b2a53fabc36
Alexandre Beduc <alexandre.beduc@gmail.com>
2025-07-01 14:13:40 +0200

    Increased Access Token and Refresh Token lifetime.

--- src/OC_projet_10/settings.py
+++ src/OC_projet_10/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 import os
 from dotenv import load_dotenv
@@ -143,6 +143,11 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES':
         ('rest_framework_simplejwt.authentication.JWTAuthentication',
          'rest_framework.authentication.SessionAuthentication',)
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
 
 # # CODE SNIPPET TO LOG DATABASE QUERIES
